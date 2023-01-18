72ed9a8d18eff5ac0f30d2b2a40b33e6bb3a3d8e
logicbyjoel <jgodinez1114@gmail.com>
2023-01-17 22:59:43 -0600

    Resolve 'duplicate symbol' error

--- src/Log.h
+++ src/Log.h
@@ -1,9 +1,4 @@
 // demonstrate how header files are processed by the compiler
-//
-
-#include <iostream>
-
-void Log(const char * message)
-{
-    std::cout << message << std::endl;
-}
+// NOTE: the linker only links object files
+// resolve 'duplicate symbol' error by removing func definition and only declaring it
+void Log(const char * message);
--- src/MyMath.cpp
+++ src/MyMath.cpp
@@ -1,7 +1,6 @@
 // create simple, lightweight source file for demo purposes
-//
-
 #include "Log.h"
+
 int Multiply(int a, int b)
 {
         Log("Multiply");
